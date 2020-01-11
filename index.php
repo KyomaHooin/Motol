6a8990005775856aeeffcfa9cffb6a9e42529b8c
KyomaHooin <richard.bruna@protonmail.com>
2020-01-11 11:41:17 +0000

    Demo code update.

--- index.php
+++ index.php
@@ -1,61 +1,98 @@
 <?php
 
-echo('<html><head><meta charset="utf-8"></head><body style="background-color:palegoldenrod;">');
+echo('<html>
+<head><meta charset="utf-8"></head>
+<body style="background-color:palegoldenrod;">');
 
 echo('
 <br>
-<p align="center"><font size="5"><b>SLUŽBY DENNÍ - Dětská - <font color="red">Laboranti</font> - obecná část, poliklinika</b></font></p>
+<p align="center">
+	<font size="5">
+		<b>SLUŽBY DENNÍ - Dětská - <font color="red">Laboranti</font> - obecná část, poliklinika</b>
+	</font>
+</p>
 
-<div align="center"><table width="60%">
+<div align="center">
+<table width="60%" style="border-collapse:collapse;">
+	<tr style="background-color:lightblue">
+	<td align="right">Zadejte termín od:</td>
+	<td>Den
+	<select>
+		<option>10</option>
+	</select>
+	</td>
+	<td>Mesic
+	<select>
+		<option>5</option>
+	</select>
+	</td>
+	<td>Rok
+	<select>
+		<option>2020</option>
+	</select>
+	</td>
+	<td><input type="checkbox">Úpravy</td>
+	<td></td>
+	</tr>
 
-<tr style="background-color:lightblue">
-<td align="right">Zadejte termín od:</td>
-<td>Den <select></select></td>
-<td>Mesic <select></select></td>
-<td>Rok <select></select></td>
-<td><input type="checkbox">Upravy</input></td>
-<td></td>
-</tr>
-<tr style="background-color:lightblue">
-<td align="right">do:</td>
-<td>Den <select>3</select></td>
-<td>Měsíc <select>2</select></td>
-<td>Rok <select>1</select></td>
-<td><button>Zobraz</button></td>
-<td><button>Tisk</button></td>
-</tr>
+	<tr style="background-color:lightblue">
+	<td align="right">do:</td>
+	<td>Den
+	<select>
+		<option>10</option>
+	</select>
+	</td>
+	<td>Měsíc
+	<select>
+		<option>15</option>
+	</select>
+	</td>
+	<td>Rok
+	<select>
+		<option>2021</option>
+	</select>
+	</td>
+	<td><button>Zobraz</button></td>
+	<td><button>Tisk</button></td>
+	</tr>
 </table>
-<br>
-<table width="60%">
-<tr style="background-color:midnightblue">
-	<td><font color="white">Datum</b></font></td>
-	<td><font color="white">LABORANT OBECNÁ (K)</b></font></td>
-	<td><font color="white">LABORANT POLIKLINIKA (L)</b></font></td>
-</tr>
 
-<tr style="background-color:lightblue;">
+<br>
+
+<table width="60%" style="border:1px solid lightgray;">
+	<tr style="background-color:midnightblue">
+	<td><font color="white">Datum</font></td>
+	<td><font color="white">LABORANT OBECNÁ (K)</font></td>
+	<td><font color="white">LABORANT POLIKLINIKA (L)</font></td>
+	</tr>
+
+	<tr style="background-color:lightblue;">
 	<td>1.8. - Pátek</td>
 	<td>Tomáš Roman</td>
 	<td>Pavel Okurka</td>
-</tr>
+	</tr>
 
-<tr style="background-color:gold;">
+	<tr style="background-color:gold;">
 	<td>2.8. - Sobota</td>
-	<td><select>
-		<option></option>
+	<td>
+	<select>
 		<option>Radek Hrouda</option>
 		<option>Jana Novotná</option>
 		<option>Zdenka Veselá</option>
-	</select></td>
+	</select>
+	</td>
 	<td>Filip Omáčka</td>
-</tr>
-</table></div>
+	</tr>
+</table>
+
+</div>
 
 <br><div align="center"><b>Vypsáno celkem: 31 dní (Maximum je 50 dní)</b></div>
 
 ');
 
-echo ('</body></html>');
+echo ('</body>
+</html>');
 
 
 ?>
